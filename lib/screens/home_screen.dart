f01c6767f85870bbf0f7f50f8062bc3842921c94
JP <sreeprakash210503@gmail.com>
2023-02-22 10:52:43 +0530

    changes some colors material

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/constraints/constraint_colors.dart';
 
 class HomeScreen extends StatelessWidget {
 const HomeScreen({ Key? key }) : super(key: key);
@@ -6,8 +7,9 @@ const HomeScreen({ Key? key }) : super(key: key);
   @override
   Widget build(BuildContext context){
     return Scaffold(
+      backgroundColor: newsBGColor,
       appBar: AppBar(
-        title: const Text('Title'),
+        title: const Text('My News Friend'),
       ),
       body: Container(),
     );
