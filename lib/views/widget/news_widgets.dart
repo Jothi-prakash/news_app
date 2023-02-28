1a2d52d960b7de85355e49072dadaaf224ed115f
JP <sreeprakash210503@gmail.com>
2023-02-28 12:00:42 +0530

    changes occur to use dubug mode

--- lib/views/widget/news_widgets.dart
+++ lib/views/widget/news_widgets.dart
@@ -11,56 +11,58 @@ const NewsWidgets({ Key? key, this.article }) : super(key: key);
 
   @override
   Widget build(BuildContext context){
-    return  Card(
-      elevation: 10,
-      child: Column(mainAxisSize: MainAxisSize.min, children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            article!.title!,
-            style: const TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            //image
-            Expanded(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(article!.source.toString()),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      article!.description!,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      article!.publishedAt!.toString(),
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  )
-                ],
-              ),
+    return  Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Card(
+        elevation: 10,
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              article!.title!,
+              style: const TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
             ),
-            Row(
-                children: [
-                  IconButton(onPressed:(() {
-                    Share.share(article!.url!);
-                  }), icon: const Icon(Icons.share))
-                ],
-            )
-          ],
-        ),
-      ]),
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(article!.source.toString()),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        article!.description!,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        article!.publishedAt!.toString(),
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Row(
+                  children: [
+                    IconButton(onPressed:(() {
+                      Share.share(article!.url!);
+                    }), icon: const Icon(Icons.share))
+                  ],
+              )
+            ],
+          ),
+        ]),
+      ),
     );
   }
 }
