993d43556b0e510b0816a494944015658907f55d
JP <sreeprakash210503@gmail.com>
2023-03-01 09:38:40 +0530

    successfully

---
+++ lib/views/news_detail_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:native_webview/native_webview.dart';
+
+class NewsDetailPage extends StatelessWidget {
+  final String? newsUrl;
+  final String? title;
+  const NewsDetailPage({Key? key, required this.newsUrl,required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:  Text(title!),
+      ),
+      body: WebView(initialUrl: newsUrl),
+    );
+  }
+}
--- lib/views/widget/news_widgets.dart
+++ lib/views/widget/news_widgets.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/views/news_detail_page.dart';
 import 'package:share_plus/share_plus.dart';
 
 import '../../model/news_headlines.dart';
@@ -12,63 +13,68 @@ class NewsWidgets extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10),
-      child: Card(
-        elevation: 10,
-        child: Column(mainAxisSize: MainAxisSize.min, children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              article!.title!,
-              style: const TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => NewsDetailPage(newsUrl: article!.url, title:article!.title),));
+        },
+        child: Card(
+          elevation: 10,
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                article!.title!,
+                style: const TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left:8.0),
-                child: article!.urlToImage != null
-                    ? Image.network(
-                        article!.urlToImage!,
-                        width: 100,
-                        height: 100,
-                      )
-                    : Image.asset(
-                        'assets/reporter.jpeg',
-                        width: 100,
-                        height: 100,
-                      ),
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        article!.description!,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        article!.getDateString(),
-                        style: const TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    )
-                  ],
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left:8.0),
+                  child: article!.urlToImage != null
+                      ? Image.network(
+                          article!.urlToImage!,
+                          width: 100,
+                          height: 100,
+                        )
+                      : Image.asset(
+                          'assets/reporter.jpeg',
+                          width: 100,
+                          height: 100,
+                        ),
                 ),
-              ),
-              IconButton(
-                  onPressed: (() {
-                    Share.share(article!.url!);
-                  }),
-                  icon: const Icon(Icons.share))
-            ],
-          ),
-        ]),
+                Expanded(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          article!.description!,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          article!.getDateString(),
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                IconButton(
+                    onPressed: (() {
+                      Share.share(article!.url!);
+                    }),
+                    icon: const Icon(Icons.share))
+              ],
+            ),
+          ]),
+        ),
       ),
     );
   }
